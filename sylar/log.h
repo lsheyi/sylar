411cd1d1e7463118bd35be55b8b50e747d3d16f2
liu shuai <1696871025@qq.com>
2023-05-08 22:06:54 +0800

    P3

--- sylar/log.h
+++ sylar/log.h
@@ -2,6 +2,10 @@
 #include <string>
 #include <stdint.h>
 #include <memory>
+#include <list>
+#include <sstream>
+#include <fstream>
+#include <iostream>
 
 namespace sylar
 {
@@ -51,9 +55,13 @@ public:
     typedef std::shared_ptr<LogAppender> ptr;
     virtual ~LogAppender(){}
 
-    void log(LogLevel::Level level, LogEvent::ptr event);
-private:
+    virtual void log(LogLevel::Level level, LogEvent::ptr event) = 0;
+    void setFormatter(LogFormatter::ptr val){ m_formatter = val; }
+    LogFormatter::ptr getFormatter() const {return m_formatter;}
+
+protected:
     LogLevel::Level m_level;
+    LogFormatter::ptr m_formatter;
 };
 
 // 日志器
@@ -61,23 +69,44 @@ class Logger{
 public:
     typedef std::shared_ptr<Logger> ptr;
     Logger(const std::string name = "root");
-
     void log(LogLevel::Level level, LogEvent::ptr event);
 
+    void debug(LogEvent::ptr event);
+    void info(LogEvent::ptr event);
+    void warn(LogEvent::ptr event);
+    void error(LogEvent::ptr event);
+    void fatal(LogEvent::ptr event);
+
+    void addAppender(LogAppender::ptr appender);
+    void delAppender(LogAppender::ptr appender);
+    LogLevel::Level getLevel() const { return m_level;}
+    void setLevel(LogLevel::Level val) { m_level = val;}
 private:
-    std::string m_name;
-    LogLevel::Level m_level;
-    LogAppender::ptr ; 
+    std::string m_name;  //日志名称
+    LogLevel::Level m_level; //日志级别
+    std::list<LogAppender::ptr> m_appenders; //Appender集合 
 };
 
 //输出到控制台的Appender
 class StdoutLogAppender : public LogAppender{
-
+public:
+    typedef std::shared_ptr<StdoutLogAppender> ptr;
+    virtual void log(LogLevel::Level level, LogEvent::ptr event) override;
+private:
 };
 
 //定义输出到文件的Appender
 class FileLogAppender : public LogAppender{
+public:
+    typedef std::shared_ptr<FileLogAppender> ptr;
+    FileLogAppender(const std::string& filename);
+    virtual void log(LogLevel::Level level, LogEvent::ptr event) override;
 
+    //重新打开文件 文件打开成功返回true
+    bool reopen();
+private:
+    std::string m_filename;
+    std::ofstream m_filestream;
 };
 
 
